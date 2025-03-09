644d4028bb41dc4baada05fcf888c7e84b06a7d1
felixlheureux <felixslheureux@gmail.com>
2025-03-09 10:26:57 -0400

    Add setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,28 +8,33 @@ MAIN_FOLDER="main"
 # Define paths
 APP_DIR="/usr/local/bin"
 CONFIG_DIR="/etc/$APP_NAME"
-LOG_DIR="/var/log/$APP_NAME"
-CRON_FILE="/etc/cron.d/$APP_NAME"
 
-# Create necessary directories
-sudo mkdir -p $CONFIG_DIR $LOG_DIR
+# install dependencies
+sudo apt update
+sudo apt install -y cron git make ufw wget
+wget https://go.dev/dl/go1.24.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
+rm -f go1.24.1.linux-amd64.tar.gz
 
-# Build the Go application
-make build
+# configure firewall
+sudo ufw default deny incoming
+sudo ufw allow OpenSSH
+sudo ufw enable -y
 
-# Move executable to the appropriate location
-sudo mv $DIST_FOLDER/$APP_NAME $APP_DIR/$APP_NAME
+# install the app
+cd ../$MAIN_FOLDER
+make
+sudo cp ../dist/$APP_NAME $APP_DIR
 sudo chmod +x $APP_DIR/$APP_NAME
-
-# Copy config and data files
-sudo cp $MAIN_FOLDER/config/prod.config.json $CONFIG_DIR/config.json
-sudo cp $MAIN_FOLDER/data/grades.json $CONFIG_DIR/grades.json
+sudo cp config/prod.config.json $CONFIG_DIR/config.json
 
 # Create a cron job to run the app every hour with the -config flag
-echo "0 * * * * root $APP_DIR/$APP_NAME -config $CONFIG_DIR/config.json >> $LOG_DIR/$APP_NAME.log 2>&1" | sudo tee $CRON_FILE
-sudo chmod 644 $CRON_FILE
-
-# Ensure cron service is running
-sudo systemctl restart cron
+echo "Creating cron job to run $APP_NAME every hour"
+echo "run crontab -e"
+echo "add to bottom of file : 0 * * * * root $APP_DIR/$APP_NAME -config $CONFIG_DIR/config.json"
+echo "run sudo systemctl enable cron"
+echo "run sudo systemctl restart cron"
 
 echo "Setup complete. $APP_NAME will run every hour."
